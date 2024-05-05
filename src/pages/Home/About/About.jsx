ed13470871b2b0a69446ae0bce79ab81547cd05e
rahiji9 <aliahmedrahi007@gmail.com>
2024-05-05 19:08:33 +0600

    third commit

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -13,10 +13,10 @@ const About = () => {
           <h3 className="text-3xl text-orange-600 font-bold">About Us</h3>
           <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
           <p className="py-6">
-          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. 
           </p>
           <p className="py-6">
-          The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.  
+          The majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.  
           </p>
           <button className="btn btn-warning">Get More Info</button>
         </div>
