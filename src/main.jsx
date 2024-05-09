0bb659e7e701d78e73a9b3b89a49ae6c45fe7167
rahiji9 <aliahmedrahi007@gmail.com>
2024-05-09 21:55:20 +0600

    buli gesi commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routes/Routes.jsx';
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+import AuthProviders from "./providers/AuthProviders.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto'>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
     <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+      <AuthProviders>
+        <RouterProvider router={router} />
+      </AuthProviders>
+    </React.StrictMode>
+    ,
   </div>
-)
+);
